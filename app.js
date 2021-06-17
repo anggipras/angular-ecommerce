7b8b7b62da27abc9834291f74e5e76bcc4f35b6b
LAPTOP-EBF7V94O\ANGGIPRASTIANTO <anggiprastianto30@gmail.com>
2021-06-17 08:29:13 +0700

    first commit be

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin: "*",
+    methods: ['GET, POST, PATCH, DELETE, PUT'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+
+// Import:Routes
+const productRoute = require('./routes/products');
+const usersRoute = require('./routes/orders');
+
+// Use Routes
+app.use('/api/products', productRoute);
+app.use('/api/users', usersRoute);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
